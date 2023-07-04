e3cecdf684742e63b7283635151311f69d560c1e
Vilson Jesuino | Dev <vilsonjesuino.dev@gmail.com>
2023-07-04 20:49:22 -0300

    Innitial commit

---
+++ src/main/java/com/example/apiremedios/ApiRemediosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiremedios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRemediosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRemediosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiremedios/ApiRemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiremedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRemediosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
